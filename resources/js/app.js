22498e5eff1f4c437f0fa7bdbf68f9f32af3c51d
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2021-05-20 00:29:04 +0700

    add some plugin again

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,8 @@
 require('./bootstrap');
-
+require('datatables.net-dt');
+require('selectize');
+require('inputmask/dist/inputmask');
+import daterangepicker from 'daterangepicker';
+window.daterangepicker = daterangepicker;
+import moment from 'moment';
+window.moment = moment;
