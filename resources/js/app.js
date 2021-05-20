c9518d42438fc6dc21f421920c40b130cf259de1
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2021-05-20 20:50:58 +0700

    add botstrap 5 and sass file for each stack holder

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,13 @@
 require('./bootstrap');
+
 require('datatables.net-dt');
 require('selectize');
 require('inputmask/dist/inputmask');
+
 import daterangepicker from 'daterangepicker';
 window.daterangepicker = daterangepicker;
+
 import moment from 'moment';
 window.moment = moment;
+
+
