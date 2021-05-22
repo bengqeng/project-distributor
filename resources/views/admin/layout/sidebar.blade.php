541a743be86a8ae90a61ca2cc859101ff5c80fe4
Deni Pratama <dnprtmptr@gmail.com>
2021-05-22 11:27:35 +0700

    fixed sidebar

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,9 +1,10 @@
- <!-- Main Sidebar Container -->
- <aside class="main-sidebar sidebar-dark-primary elevation-4">
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-light-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
-    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
+  <a href="/admin" class="brand-link">
+    <img src="/img/admin/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      style="opacity: .8">
+    <span class="brand-text font-weight-light">Logo Brand</span>
   </a>
 
   <!-- Sidebar -->
@@ -11,10 +12,10 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <img src="/img/admin/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="#" class="d-block">Alexander Pierce</a>
+       Alexander Pierce 
       </div>
     </div>
 
@@ -35,35 +36,62 @@
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
              with font-awesome or any other icon font library -->
-        <li class="nav-item menu-open">
-          <a href="#" class="nav-link active">
-            <i class="nav-icon fas fa-tachometer-alt"></i>
+        <li class="nav-item">
+          <a href="/admin/webcontent" class="nav-link">
+            <i class="nav-icon fas fa-calendar-alt"></i>
             <p>
-              Starter Pages
-              <i class="right fas fa-angle-left"></i>
+              Web Content
             </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-book"></i>
+            <p>
+              Users
+              <i class="fas fa-angle-left right"></i>
+            </p>
+            <span class="badge badge-info right">New 3</span>
           </a>
           <ul class="nav nav-treeview">
             <li class="nav-item">
-              <a href="#" class="nav-link active">
+              <a href="/admin/users" class="nav-link">
                 <i class="far fa-circle nav-icon"></i>
-                <p>Active Page</p>
+                <p>All</p>
+                <span class="badge badge-info right">3</span>
               </a>
             </li>
+          </ul>
+          <ul class="nav nav-treeview">
             <li class="nav-item">
-              <a href="#" class="nav-link">
+              <a href="/admin/users/approval" class="nav-link">
                 <i class="far fa-circle nav-icon"></i>
-                <p>Inactive Page</p>
+                <p>Aproval</p>
+              </a>
+            </li>
+          </ul>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="/admin/users/deleted" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Deleted</p>
               </a>
             </li>
           </ul>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-th"></i>
+          <a href="/admin/upload" class="nav-link">
+            <i class="nav-icon far fa-image"></i>
             <p>
-              Simple Link
-              <span class="right badge badge-danger">New</span>
+              Upload Image
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/admin/graphic" class="nav-link">
+            <i class="nav-icon fas fa-chart-pie"></i>
+            <p>
+              Graphic
             </p>
           </a>
         </li>
