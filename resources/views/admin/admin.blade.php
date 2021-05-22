ef5ef647fe83467c93473b1af3e888687d380d01
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2021-05-22 13:00:42 +0700

    fix tamplate

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -13,6 +13,7 @@
   </head>
 
   <body class="hold-transition sidebar-mini">
+    
     <div class="wrapper">
 
       @include('admin.layout.sidebar')
@@ -24,16 +25,6 @@
         @yield('main-content')
       </div>
       <!-- /.content-wrapper -->
-
-      <!-- Control Sidebar -->
-      <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-        <div class="p-3">
-          <h5>Title</h5>
-          <p>Sidebar content</p>
-        </div>
-      </aside>
-      <!-- /.control-sidebar -->
 
       @include('admin.layout.footer')
     </div>
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
   <!-- Brand Logo -->
   <a href="/admin" class="brand-link">
-    <img src="/img/admin/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <img src="/vendor/img/admin/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
       style="opacity: .8">
     <span class="brand-text font-weight-light">Logo Brand</span>
   </a>
@@ -12,7 +12,7 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="/img/admin/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <img src="/vendor/img/admin/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
        Alexander Pierce 
