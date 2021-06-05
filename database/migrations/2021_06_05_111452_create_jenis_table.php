d64cda1f5aa86d98dcc78a97ca347b745b215d37
Adrian Rahmandanu <adrian.rahmandnanu2@gmail.com>
2021-06-05 19:23:59 +0700

    add migration for wilayah indonesia

---
+++ database/migrations/2021_06_05_111452_create_jenis_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJenisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("
+            CREATE TABLE IF NOT EXISTS jenis (
+                id_jenis int(11) NOT NULL,
+                nama tinytext NOT NULL
+            );
+        ");
+
+        DB::statement("
+            INSERT INTO jenis (id_jenis, nama) VALUES
+            (1, 'kabupaten'),
+            (2, 'kota'),
+            (3, 'kelurahan'),
+            (4, 'desa');
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jenis');
+    }
+}
