efc43d302f23a47ab1a8e7f88e1fc9fc70b53ce4
Naufal <muh.naufal@hotmail.com>
2021-06-09 23:03:43 +0700

    build BE news

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\News;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class NewsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = News::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(10),
+            'content' => $this->faker->realText(1000),
+            'author' => $this->faker->name(10),
+            'image' => $this->faker->randomDigit(),
+        ];
+    }
+}
--- resources/views/landingpage/news/all.blade.php
+++ resources/views/landingpage/news/all.blade.php
@@ -5,17 +5,17 @@
   </div>
   <div class="container">
     <h3 class="text-white font-weight-bolder mb-4">NEWS</h3>
-    @for ($i = 0; $i < 4; $i++) <div class="card mb-3" style="">
+    @forelse ($news as $items)
+    <div class="card mb-3" style="">
       <div class="row no-gutters">
         <div class="col-md-4 p-3">
           <img src="//placehold.it/1000x700" class="card-img" alt="...">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text mb-3">Author</p>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-              content.This content is a little bit longer.</p>
+            <h5 class="card-title">{{$items->title}}</h5>
+            <p class="card-text mb-3">{{$items->author}}</p>
+            <p class="card-text">{{Str::limit($items->content, 150, $end='...')}}</p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             <div class="d-flex h-100 d-flex flex-column">
               <a role="button" href="#" class="align-self-end btn-sm btn-our-grey float-right">READ MORE</a>
@@ -23,8 +23,25 @@
           </div>
         </div>
       </div>
+    </div>
+    @empty
+    <div class="card mb-3" style="">
+      <div class="row no-gutters">
+        <div class="card-body text-center p-5">
+          <h5 class="card-title">Ups.. Maaf, halaman News masih dalam penulisan</h5>
+          <p class="card-text mb-3">Mimin Cakep</p>
+          <p class="card-text">Mohon ditunggu ya kabar selanjutnya...
+            tetap cantik.
+          </p>
+          <p class="card-text"><small class="text-muted">Last updated hmm mins ago</small></p>
+        </div>
+      </div>
+    </div>
   </div>
-  @endfor
+  @endforelse
+  <div class="d-flex justify-content-center">
+    {!! $news->links() !!}
+  </div>
 </div>
 </div>
 @endsection
