146b90ebaec3d59261179b0937fe96dc1e9076f8
Naufal <muh.naufal@hotmail.com>
2021-06-09 23:19:56 +0700

    fixing page about responsive

--- resources/views/landingpage/gallery.blade.php
+++ resources/views/landingpage/gallery.blade.php
@@ -4,9 +4,9 @@
     <div class="container">
         <h1 class="text-white font-weight-bolder mt-3 ml-5 mb-4">Gallery</h1>
         <div class="container">
-            <div class="col-sm-12 justify-content-center mb-4 grid">
-                <div class="row">
-                    @for ($i = 0; $i < 25; $i++) <div class="col-sm-2 grid-item m-1 border-1 p-0">
+            <div class="col-lg-12 mb-4 grid">
+                <div class="row justify-content-center text-center align-content-center">
+                    @for ($i = 0; $i < 25; $i++) <div class="col-lg-2 grid-item m-1 border-1 p-0">
                         <div class="card text-center border-0" style="">
                             <img src="https://via.placeholder.com/800x{{mt_rand(500,800)}}" class="card-img-top"
                                 alt="...">
--- resources/views/landingpage/product/category.blade.php
+++ resources/views/landingpage/product/category.blade.php
@@ -5,41 +5,18 @@
         <h2 class="text-white font-weight mt-3 ml-5">SHOP BY CATEGORY</h2>
         <h3 class="text-white font-weight-bolder mt-1 ml-5 mb-5">BODY SERIES</h3>
         <div class="container-fluid">
-            <div class="d-flex justify-content-center p-5">
-                <div class="col-3 py-3 px-4">
+            <div class="row justify-content-center p-5">
+                @for ($i = 0; $i < 50; $i++) <div class="col-lg-3 py-3 px-4">
                     <div class="card text-white">
                         <img class="card-img" src="https://via.placeholder.com/285x400" alt="Card image">
                         <div class="card-img-overlay h-100 d-flex flex-column justify-content-end">
                             <button class="align-self-center btn-sm btn-our-grey">View Product</button>
                         </div>
                     </div>
-                </div>
-                <div class="col-3 py-3 px-4">
-                    <div class="card text-white">
-                        <img class="card-img" src="https://via.placeholder.com/285x400" alt="Card image">
-                        <div class="card-img-overlay h-100 d-flex flex-column justify-content-end">
-                            <button class="align-self-center btn-sm btn-our-grey">View Product</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-3 py-3 px-4">
-                    <div class="card text-white">
-                        <img class="card-img" src="https://via.placeholder.com/285x400" alt="Card image">
-                        <div class="card-img-overlay h-100 d-flex flex-column justify-content-end">
-                            <button class="align-self-center btn-sm btn-our-grey">View Product</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-3 py-3 px-4">
-                    <div class="card text-white">
-                        <img class="card-img" src="https://via.placeholder.com/285x400" alt="Card image">
-                        <div class="card-img-overlay h-100 d-flex flex-column justify-content-end">
-                            <button class="align-self-center btn-sm btn-our-grey">View Product</button>
-                        </div>
-                    </div>
-                </div>
             </div>
+            @endfor
         </div>
     </div>
+</div>
 </div>
 @endsection
