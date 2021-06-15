22cc4e81ac2ed8f5c1125454dcd46ace99a849a9
Adrian Rahmandanu <adrian.rahmandnanu2@gmail.com>
2021-06-15 18:28:38 +0700

    fix indent and naming

--- resources/views/landingpage/gallery.blade.php
+++ resources/views/landingpage/gallery.blade.php
@@ -7,20 +7,21 @@
         <div class="container">
             <div class="col-lg-12 mb-4 grid">
                 <div class="row justify-content-center text-center align-content-center">
-                    @for ($i = 0; $i < 25; $i++) <div class="col-lg-2 grid-item m-1 border-1 p-0">
-                        <div class="card text-center border-0" style="">
-                            <img src="https://via.placeholder.com/800x{{mt_rand(500,800)}}" class="card-img-top"
-                                alt="...">
-                            {{-- <div class="card-body bg-our-white">
-                                            <h5 class="card-title">Card title</h5>
-                                            <a href="#" class="btn btn-sm btn-our-grey">Go somewhere</a>
-                                        </div> --}}
+                    @for ($i = 0; $i < 25; $i++)
+                        <div class="col-lg-2 grid-item m-1 border-1 p-0">
+                            <div class="card text-center border-0" style="">
+                                <img src="https://via.placeholder.com/800x{{mt_rand(500,800)}}" class="card-img-top"
+                                    alt="...">
+                                {{-- <div class="card-body bg-our-white">
+                                                <h5 class="card-title">Card title</h5>
+                                                <a href="#" class="btn btn-sm btn-our-grey">Go somewhere</a>
+                                            </div> --}}
+                            </div>
                         </div>
+                    @endfor
                 </div>
-                @endfor
             </div>
         </div>
     </div>
-</div>
 </div>
 @endsection
