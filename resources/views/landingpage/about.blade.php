dd83c03baf68949dad6f2abd3033b5500de818ef
muh.naufal@hotmail.com <muh.naufal@hotmail.com>
2021-06-22 18:53:12 +0700

    lol

--- resources/views/landingpage/about.blade.php
+++ resources/views/landingpage/about.blade.php
@@ -32,7 +32,7 @@
                         It uses a dictionary of over 200 Latin words, combined with a handful of model sentence
                         structures, to generate Lorem Ipsum which looks reasonable.
                         The generated Lorem Ipsum is therefore always free from repetition, injected humour, or
-                        non-characteristic words etc.
+                        non-characteristic words etc.asd
                     </p>
                 </div>
             </div>
