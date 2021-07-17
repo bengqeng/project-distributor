8a19563fb943021bbf7263861572c6fe7f351f29
Adrian Rahmandanu <adrian.rahmandnanu2@gmail.com>
2021-07-18 00:38:41 +0700

    change format notification

--- resources/views/emails/users/rejected_notification_plain.blade.php
+++ resources/views/emails/users/rejected_notification_plain.blade.php
@@ -1,5 +1,5 @@
-<p>{{ env('APP_NAME') }}</p>
-<p>Nama Lengkap         : {{ $user->full_name }}</p>
-<p>No Telephone         : {{ $user->phone_number }}</p>
-<p>Akun Id(username)    : {{ $user->username }}</p>
-<p>Telah ditolak menjadi member</p>
+{{ env('APP_NAME') }}
+Nama Lengkap         : {{ $user->full_name }}
+No Telephone         : {{ $user->phone_number }}
+Akun Id(username)    : {{ $user->username }}
+Telah ditolak menjadi member
